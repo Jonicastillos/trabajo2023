110cd05868adf497fa9cf5a79215e4117a615f74
Jonicastillos <126710697+Jonicastillos@users.noreply.github.com>
2023-03-01 22:17:45 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById('van').onclick = function () {
+    console.log("click");
+    document.getElementById("demo").innerHTML ="leer mas";
+    
+  }
+  document.getElementById('van1').onclick = function () {
+    console.log("click");
+    document.getElementById("p1").innerHTML ="leer mas";
+    
+  }
+  document.getElementById('van2').onclick = function () {
+    console.log("click");
+    document.getElementById("p2").innerHTML = "leer mas";
+    
+  } 
+  document.getElementById('van3').onclick = function () {
+    console.log("click");
+    document.getElementById("p3").innerHTML = "leer mas";
+    
+  } 
+  document.getElementById('van4').onclick = function () {
+    console.log("click");
+    document.getElementById("p4").innerHTML = "leer mas";
+    
+  } 
+  document.getElementById('van5').onclick = function () {
+    console.log("click");
+    document.getElementById("p5").innerHTML = "leer mas";
+    
+  } 
+ 
